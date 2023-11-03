86bfe00b147d0f6ee556cd980c9cecc1ec767985
Ayushi <ayushimanoj1201@gmail.com>
2023-11-03 10:29:06 +0530

    Loading UI

--- app/root.tsx
+++ app/root.tsx
@@ -9,6 +9,7 @@ import {
   ScrollRestoration,
   NavLink,
   useLoaderData,
+  useNavigation,
 } from "@remix-run/react";
 import appStylesHref from "./app.css";
 import { json, redirect } from "@remix-run/node";
@@ -31,6 +32,7 @@ export const links: LinksFunction = () => [
 
 export default function App() {
   const { contacts } = useLoaderData<typeof loader>();
+  const navigation = useNavigation();
 
   return (
     <html lang="en">
@@ -88,7 +90,10 @@ export default function App() {
             )}
           </nav>
         </div>
-        <div id="detail">
+        <div
+          id="detail"
+          className={navigation.state === "loading" ? "loading" : ""}
+        >
           <Outlet />
         </div>
 
