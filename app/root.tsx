f94065badf3f8cd02a11ae1e9213a03ea4befa3d
Ayushi <ayushimanoj1201@gmail.com>
2023-11-03 10:23:50 +0530

    Active Link Styling

--- app/root.tsx
+++ app/root.tsx
@@ -7,7 +7,7 @@ import {
   Outlet,
   Scripts,
   ScrollRestoration,
-  Link,
+  NavLink,
   useLoaderData,
 } from "@remix-run/react";
 import appStylesHref from "./app.css";
@@ -63,7 +63,12 @@ export default function App() {
               <ul>
                 {contacts.map((contact) => (
                   <li key={contact.id}>
-                    <Link to={`contacts/${contact.id}`}>
+                    <NavLink
+                      className={({ isActive, isPending }) =>
+                        isActive ? "active" : isPending ? "pending" : ""
+                      }
+                      to={`contacts/${contact.id}`}
+                    >
                       {contact.first || contact.last ? (
                         <>
                           {contact.first} {contact.last}
@@ -72,7 +77,7 @@ export default function App() {
                         <i>No Name</i>
                       )}{" "}
                       {contact.favorite ? <span>★</span> : null}
-                    </Link>
+                    </NavLink>
                   </li>
                 ))}
               </ul>
